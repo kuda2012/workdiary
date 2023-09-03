d423a14e6112547b8ed4ea2bc043ef1a42fd5e94
kuda2012 <kuda2012@gmail.com>
2023-09-02 19:45:18 -0500

    popup window opens and closes and react is functioning in localhost and popup

---
+++ frontend_chrome/public/background.js
@@ -0,0 +1,42 @@
+let windowId;
+async function openPopup() {
+  chrome.windows.create(
+    {
+      url: "index.html", // Replace with your HTML file's path
+      type: "popup",
+      width: 800, // Set the width and height as desired
+      height: 600,
+      top: 100, // Adjust the window's position as needed
+      left: 100,
+    },
+    function (window) {
+      windowId = window.id;
+    }
+  );
+}
+
+function closePopup() {
+  console.log("removing");
+  chrome.windows.remove(windowId, function () {
+    windowId = undefined;
+    console.log("should be empty1", windowId);
+  });
+}
+
+chrome.action.onClicked.addListener(function () {
+  console.log("onclick", windowId);
+  if (!windowId) {
+    openPopup();
+  } else {
+    closePopup();
+  }
+});
+
+chrome.windows.onRemoved.addListener(function () {
+  // Handle window removal here
+  console.log("Popup window with ID " + windowId + " was closed.");
+  windowId = undefined;
+  console.log("should be empty2", windowId);
+
+  // Perform any necessary cleanup or tasks
+});
---
+++ frontend_chrome/src/components/Oauth.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  console.log("This is a popup!");
+
+  return <>Yo</>;
+}
+
+export default App;
