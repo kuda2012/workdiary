e396a58f7a2005012e000331191415d152b4ad02
kuda2012 <kuda2012@gmail.com>
2023-10-12 23:25:47 -0500

    prelim canRecord, now to send recording off to backend

--- frontend_chrome/src/components/HomeLoggedIn.jsx
+++ frontend_chrome/src/components/HomeLoggedIn.jsx
@@ -4,15 +4,11 @@ import "react-quill/dist/quill.snow.css";
 import SummaryTextArea from "./SummaryTextArea";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
-import {
-  getPost,
-  updatePost,
-  createPost,
-  createTag,
-} from "../helpers/actionCreators";
+import { getPost, updatePost, createPost } from "../helpers/actionCreators";
 import moment from "moment";
 import Tabs from "./Tabs";
 import Tags from "./Tags";
+import SummaryVoice from "./SummaryVoice";
 
 const HomeLoggedIn = () => {
   const post = useSelector((state) => state.post);
@@ -39,7 +35,6 @@ const HomeLoggedIn = () => {
       setFormerDate(date);
     }
   }
-
   return (
     <div className="container center-row">
       <div className="row justify-content-between">
@@ -50,6 +45,7 @@ const HomeLoggedIn = () => {
           {date && formerDate === date && (
             <>
               <Tags />
+              <SummaryVoice />
               <SummaryTextArea
                 initialContent={post?.summary_text}
                 dispatchUpdatePost={dispatchUpdatePost}
