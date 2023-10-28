760f87cdd25769355ed0a5d066f4c632b3ccd176
kuda2012 <kuda2012@gmail.com>
2023-10-27 22:19:02 -0500

    some styling

---
+++ frontend_chrome/src/components/CustomToolbar.jsx
@@ -0,0 +1,25 @@
+const CustomToolBar = ({ openTagsModal }) => {
+  return (
+    <div id="toolbar">
+      <select
+        className="ql-header"
+        defaultValue={""}
+        onChange={(e) => e.persist()}
+      >
+        <option value="1" />
+        <option value="2" />
+        <option value="" selected />
+      </select>
+      <button className="ql-bold" />
+      <button className="ql-italic" />
+      <button className="ql-strike" />
+      <button className="ql-list" value="ordered" />
+      <button className="ql-list" value="bullet" />
+      <button className="showTags" onClick={openTagsModal}>
+        <b>Tags</b>
+      </button>
+    </div>
+  );
+};
+
+export default CustomToolBar;
---
+++ frontend_chrome/src/components/TagsModal.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "../styles/TagsModal.css"; // You can create your own CSS for styling
+
+const TagsModal = ({ isTagsModalOpen, closeTagsModal, children }) => {
+  if (!isTagsModalOpen) return null;
+  const handleOverlayClick = (e) => {
+    if (e.target === e.currentTarget) {
+      closeTagsModal();
+    }
+  };
+  return isTagsModalOpen
+    ? ReactDOM.createPortal(
+        <div className="tags-modal-overlay" onClick={handleOverlayClick}>
+          <div className="tags-modal">{children}</div>
+        </div>,
+        document.body
+      )
+    : null;
+};
+
+export default TagsModal;
