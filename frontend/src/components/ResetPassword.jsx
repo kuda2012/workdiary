958a613322703df09a00ac0a5c4833d1258028ef
kuda2012 <kuda2012@gmail.com>
2024-01-07 12:46:46 -0600

    Password reset copy changes

--- frontend/src/components/ResetPassword.jsx
+++ frontend/src/components/ResetPassword.jsx
@@ -13,7 +13,7 @@ const ResetPassword = ({}) => {
     const { name, value } = e.target;
     setFormData((data) => ({ ...data, [name]: value }));
   };
-
+  const [resetDone, setResetDone] = useState(false);
   const [showPassword1, setShowPassword1] = useState(false);
   const [showPassword2, setShowPassword2] = useState(false);
   const toggleShowPassword1 = () => {
@@ -38,6 +38,7 @@ const ResetPassword = ({}) => {
           { headers: { Authorization: `Bearer ${resetToken}` } }
         );
         alert(response?.data?.message);
+        setResetDone(true);
         setFormData(INITIAL_STATE);
       } else {
         alert("Passwords must match");
@@ -45,65 +46,71 @@ const ResetPassword = ({}) => {
       }
     } catch (error) {
       setFormData(INITIAL_STATE);
-      alert(error?.response?.data?.message);
+      alert(error?.response?.data?.message || error?.message);
     }
   };
 
   return (
-    <div className="card mt-3" style={{ width: "600px" }}>
-      <div className="card-body">
-        <form onSubmit={handleSubmit}>
-          <div className="reset-password-form-items form-group">
-            <label htmlFor="password" className="mb-2">
-              <h5>Reset Password</h5>
-            </label>
-            <div className="input-password">
-              <input
-                autoComplete="password-copy"
-                placeholder="New Password"
-                id="new_password"
-                type={showPassword1 ? "text" : "password"}
-                required={true}
-                name="new_password"
-                className="form-control signup-password mb-2"
-                onChange={handleChange}
-                value={formData.new_password}
-              />
-              <button
-                type="button"
-                className="password-button mb-2"
-                onClick={() => toggleShowPassword1()}
-              >
-                {showPassword1 ? "Hide" : "Show"}
-              </button>
-            </div>
-            <div className="input-password">
-              <input
-                autoComplete="password-copy"
-                id="new_password_copy"
-                placeholder="Enter new password again"
-                type={showPassword2 ? "text" : "password"}
-                required={true}
-                name="new_password_copy"
-                className="form-control signup-password"
-                onChange={handleChange}
-                value={formData.new_password_copy}
-              />
-              <button
-                type="button"
-                className="password-button"
-                onClick={() => toggleShowPassword2()}
-              >
-                {showPassword2 ? "Hide" : "Show"}
-              </button>
-            </div>
-            <Button color="primary" className="reset-password-submit mt-4">
-              Submit
-            </Button>
+    <>
+      {!resetDone ? (
+        <div className="card mt-3" style={{ width: "600px" }}>
+          <div className="card-body">
+            <form onSubmit={handleSubmit}>
+              <div className="reset-password-form-items form-group">
+                <label htmlFor="password" className="mb-2">
+                  <h5>Reset Password</h5>
+                </label>
+                <div className="input-password">
+                  <input
+                    autoComplete="password-copy"
+                    placeholder="New Password"
+                    id="new_password"
+                    type={showPassword1 ? "text" : "password"}
+                    required={true}
+                    name="new_password"
+                    className="form-control signup-password mb-2"
+                    onChange={handleChange}
+                    value={formData.new_password}
+                  />
+                  <button
+                    type="button"
+                    className="password-button mb-2"
+                    onClick={() => toggleShowPassword1()}
+                  >
+                    {showPassword1 ? "Hide" : "Show"}
+                  </button>
+                </div>
+                <div className="input-password">
+                  <input
+                    autoComplete="password-copy"
+                    id="new_password_copy"
+                    placeholder="Enter new password again"
+                    type={showPassword2 ? "text" : "password"}
+                    required={true}
+                    name="new_password_copy"
+                    className="form-control signup-password"
+                    onChange={handleChange}
+                    value={formData.new_password_copy}
+                  />
+                  <button
+                    type="button"
+                    className="password-button"
+                    onClick={() => toggleShowPassword2()}
+                  >
+                    {showPassword2 ? "Hide" : "Show"}
+                  </button>
+                </div>
+                <Button color="primary" className="reset-password-submit mt-4">
+                  Submit
+                </Button>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
-    </div>
+        </div>
+      ) : (
+        <h5>Your password has been reset. You can return to the app now :)</h5>
+      )}
+    </>
   );
 };
 
