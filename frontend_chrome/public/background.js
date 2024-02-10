869635d9b62df495408283e3fe2f4766f4d82a06
kuda2012 <kuda2012@gmail.com>
2024-02-10 16:15:10 -0600

    alarm issues and stop double popups when click on notificaton

--- frontend_chrome/public/background.js
+++ frontend_chrome/public/background.js
@@ -48,13 +48,55 @@ chrome.action.onClicked.addListener(function () {
   togglePopup();
 });
 
-chrome.windows.onRemoved.addListener(function (windowId) {
+chrome.windows.onRemoved.addListener(async function (windowId) {
   if (popupWindow && popupWindow.id === windowId) {
     popupWindow = undefined;
+    await chrome.storage.session.remove("action_creator_alarm_set");
+    setUpBackground();
   }
 });
 
-chrome.runtime.onInstalled.addListener(async () => {
+async function setAlarm(user) {
+  await chrome.alarms.clearAll();
+  if (!user.alarm_status) return;
+
+  const DAYS_OF_WEEK = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+  // Get the current day's index
+  const currentDayIndex = DAYS_OF_WEEK.indexOf(
+    new Date().toLocaleString("en-us", { weekday: "short" }).toLowerCase()
+  );
+
+  // Get the current time
+  const currentTime = new Date();
+
+  // Set alarms for today and the next 6 days
+  for (let i = currentDayIndex; i < currentDayIndex + 7; i++) {
+    const day = DAYS_OF_WEEK[i % 7]; // Handle wrapping around to the beginning of the week
+    if (user.alarm_days.find((dayObj) => dayObj.day === day && dayObj.value)) {
+      // Check if the day is enabled
+
+      // Set alarm for this day
+      const nextOccurrence = new Date();
+      nextOccurrence.setDate(nextOccurrence.getDate() + (i - currentDayIndex)); // Adjust for the current day
+
+      // Set the time based on the user's specified alarm time
+      nextOccurrence.setHours(user.alarm_time.split(":")[0]);
+      nextOccurrence.setMinutes(user.alarm_time.split(":")[1]);
+      nextOccurrence.setSeconds(0);
+
+      // If the alarm time for today has already passed, set the alarm for the same day next week
+      if (currentTime >= nextOccurrence) {
+        nextOccurrence.setDate(nextOccurrence.getDate() + 7);
+      }
+      console.log("background", nextOccurrence, day);
+
+      chrome.alarms.create(`myAlarm_${day}`, {
+        when: nextOccurrence.getTime(),
+      });
+    }
+  }
+}
+async function setUpBackground() {
   async function isWorkdiaryTokenCurrent(workdiary_token) {
     try {
       const response = await fetch(
@@ -75,7 +117,28 @@ chrome.runtime.onInstalled.addListener(async () => {
       console.log(error);
     }
   }
+  await chrome.storage.local.get(["workdiary_token"]).then(async (result) => {
+    const workdiaryToken =
+      result?.workdiary_token && typeof result?.workdiary_token === "string"
+        ? await isWorkdiaryTokenCurrent(result.workdiary_token)
+        : null;
+    let response = null;
+    if (workdiaryToken) {
+      response = await fetch(`${config.LOCAL_BACKEND_URL}/users/account-info`, {
+        method: "GET",
+        headers: { Authorization: `Bearer ${workdiaryToken}` },
+      });
+      const { user } = response ? await response.json() : null;
+      if (user) {
+        chrome.storage.session.set({ user });
+        await setAlarm(user);
+      }
+    }
+  });
+}
 
+chrome.runtime.onInstalled.addListener(async () => {
+  setUpBackground();
   chrome.tabs.onCreated.addListener(function (tab) {
     // Send a message to alert the extension about the new tab
     if (
@@ -104,25 +167,9 @@ chrome.runtime.onInstalled.addListener(async () => {
       });
     }
   });
-  await chrome.storage.local.get(["workdiary_token"]).then(async (result) => {
-    const workdiaryToken =
-      result?.workdiary_token && typeof result?.workdiary_token === "string"
-        ? await isWorkdiaryTokenCurrent(result.workdiary_token)
-        : null;
-    let response = null;
-    if (workdiaryToken) {
-      response = await fetch(`${config.LOCAL_BACKEND_URL}/users/account-info`, {
-        method: "GET",
-        headers: { Authorization: `Bearer ${workdiaryToken}` },
-      });
-      const { user } = response ? await response.json() : null;
-      if (user) {
-        chrome.storage.session.set({ user });
-        await setAlarm(user);
-      }
-    }
+  chrome.notifications.onClicked.addListener(() => {
+    togglePopup();
   });
-
   chrome.alarms.onAlarm.addListener(async (alarm) => {
     const { user } = await chrome.storage.session.get(["user"]);
     const { action_creator_alarm_set } = await chrome.storage.session.get([
@@ -143,59 +190,8 @@ chrome.runtime.onInstalled.addListener(async () => {
         message: `Reminder to write in your Workdiary!`,
         // Include sound property for the sound file
       });
-      chrome.notifications.onClicked.addListener(() => {
-        openPopup();
-      });
+
       await setAlarm(user);
     }
   });
-
-  chrome.windows.onRemoved.addListener(function () {
-    emove("action_creator_alarm_set");
-  });
-
-  async function setAlarm(user) {
-    await chrome.alarms.clearAll();
-    if (!user.alarm_status) return;
-
-    const DAYS_OF_WEEK = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
-    // Get the current day's index
-    const currentDayIndex = DAYS_OF_WEEK.indexOf(
-      new Date().toLocaleString("en-us", { weekday: "short" }).toLowerCase()
-    );
-
-    // Get the current time
-    const currentTime = new Date();
-
-    // Set alarms for today and the next 6 days
-    for (let i = currentDayIndex; i < currentDayIndex + 7; i++) {
-      const day = DAYS_OF_WEEK[i % 7]; // Handle wrapping around to the beginning of the week
-      if (
-        user.alarm_days.find((dayObj) => dayObj.day === day && dayObj.value)
-      ) {
-        // Check if the day is enabled
-
-        // Set alarm for this day
-        const nextOccurrence = new Date();
-        nextOccurrence.setDate(
-          nextOccurrence.getDate() + (i - currentDayIndex)
-        ); // Adjust for the current day
-
-        // Set the time based on the user's specified alarm time
-        nextOccurrence.setHours(user.alarm_time.split(":")[0]);
-        nextOccurrence.setMinutes(user.alarm_time.split(":")[1]);
-        nextOccurrence.setSeconds(0);
-
-        // If the alarm time for today has already passed, set the alarm for the same day next week
-        if (currentTime >= nextOccurrence) {
-          nextOccurrence.setDate(nextOccurrence.getDate() + 7);
-        }
-        console.log("background", nextOccurrence, day);
-
-        chrome.alarms.create(`myAlarm_${day}`, {
-          when: nextOccurrence.getTime(),
-        });
-      }
-    }
-  }
 });
