a43a0b3da2ae134521cfa5902abeb28cc71f6d45
kuda2012 <kuda2012@gmail.com>
2024-02-12 00:46:19 -0600

    revert verifyAccount

--- frontend/src/components/VerifyAccount.jsx
+++ frontend/src/components/VerifyAccount.jsx
@@ -5,6 +5,8 @@ import "../styles/VerifyAccount.css";
 const { VITE_LOCAL_BACKEND_URL } = import.meta.env;
 
 const VerifyAccount = ({}) => {
+  const [accountVerificationCalled, setAccountVerificationCalled] =
+    useState(false);
   const [verificationMessage, setVerificationMessage] = useState("");
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
@@ -12,22 +14,26 @@ const VerifyAccount = ({}) => {
   useEffect(() => {
     async function fetchData() {
       try {
-        const { data } = await axios.get(
-          `${VITE_LOCAL_BACKEND_URL}/users/verify-account?token=${verificationToken}`
-        );
-        if (data.message) {
-          setVerificationMessage(data.message);
+        if (!accountVerificationCalled) {
+          const { data } = await axios.get(
+            `${VITE_LOCAL_BACKEND_URL}/users/verify-account?token=${verificationToken}`
+          );
+          setAccountVerificationCalled(true);
+          if (data.message) {
+            setVerificationMessage(data.message);
+          }
         }
       } catch (error) {
         alert(error?.response?.data?.message || error?.message);
         // prevents another request to the backend
+        setAccountVerificationCalled(true);
         setVerificationMessage(
           error?.response?.data?.message || error?.message
         );
       }
     }
     fetchData();
-  });
+  }, []);
   return (
     <>
       <h5 className="mt-2" id="verify-account">
