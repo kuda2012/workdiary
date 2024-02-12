654167382cb2b7cc67374c497d49a4a5b1a0a356
kuda2012 <kuda2012@gmail.com>
2024-02-12 00:16:38 -0600

    account verificaiton test

--- frontend/src/components/VerifyAccount.jsx
+++ frontend/src/components/VerifyAccount.jsx
@@ -5,8 +5,6 @@ import "../styles/VerifyAccount.css";
 const { VITE_LOCAL_BACKEND_URL } = import.meta.env;
 
 const VerifyAccount = ({}) => {
-  const [accountVerificationCalled, setAccountVerificationCalled] =
-    useState(false);
   const [verificationMessage, setVerificationMessage] = useState("");
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
@@ -14,19 +12,16 @@ const VerifyAccount = ({}) => {
   useEffect(() => {
     async function fetchData() {
       try {
-        if (!accountVerificationCalled) {
-          const { data } = await axios.get(
-            `${VITE_LOCAL_BACKEND_URL}/users/verify-account?token=${verificationToken}`
-          );
-          setAccountVerificationCalled(true);
-          if (data.message) {
-            setVerificationMessage(data.message);
-          }
+        const { data } = await axios.get(
+          `${VITE_LOCAL_BACKEND_URL}/users/verify-account?token=${verificationToken}`
+        );
+        console.log(data);
+        if (data.message) {
+          setVerificationMessage(data.message);
         }
       } catch (error) {
         alert(error?.response?.data?.message || error?.message);
         // prevents another request to the backend
-        setAccountVerificationCalled(true);
         setVerificationMessage(
           error?.response?.data?.message || error?.message
         );
