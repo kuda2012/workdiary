80bb50629cd866d0f4da0dcdc7a7a75d9a45aa28
kuda2012 <kuda2012@gmail.com>
2024-02-14 18:44:54 -0600

    test

--- backend/models/User.js
+++ backend/models/User.js
@@ -98,7 +98,7 @@ class User {
       { id: user.id, email: user.email },
       VERIFY_ACCOUNT_SECRET_KEY,
       {
-        expiresIn: "20m",
+        expiresIn: "1m",
       }
     );
     // Email options
