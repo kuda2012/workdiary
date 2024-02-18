fc289ea36d3a3b4663537c7b76cd48676f99d4ff
kuda2012 <kuda2012@gmail.com>
2024-02-17 22:18:32 -0600

    test

--- backend/helpers/databaseJob.js
+++ backend/helpers/databaseJob.js
@@ -1,5 +1,6 @@
 const { db } = require("../db");
 async function databaseJob() {
+  console.log("corny", db);
   await db.query(`DELETE FROM users
                   WHERE verified = false
                   AND created_at < current_timestamp - interval '1 day'`);
