43bb634d04db237e108463283b469b7352f53597
kuda2012 <kuda2012@gmail.com>
2024-02-24 22:31:31 -0600

    update toggle

--- frontend_chrome/src/components/OtherSettings.jsx
+++ frontend_chrome/src/components/OtherSettings.jsx
@@ -66,11 +66,11 @@ const OtherSettings = () => {
             <div class="accordion-item">
               <h2 class="accordion-header" id="headingTwo">
                 <button
-                  class="accordion-button"
+                  class="accordion-button collapsed"
                   type="button"
                   data-bs-toggle="collapse"
                   data-bs-target="#collapseTwo"
-                  aria-expanded="true"
+                  aria-expanded="false"
                   aria-controls="collapseTwo"
                 >
                   Sound effects
