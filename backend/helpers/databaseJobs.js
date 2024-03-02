9b09761ae1d306d3b71abdb97c39813dfe6f6350
kuda2012 <kuda2012@gmail.com>
2024-03-01 18:36:29 -0600

    rename database job

--- backend/helpers/databaseJobs.js
+++ backend/helpers/databaseJobs.js
@@ -1,9 +1,9 @@
 const { db } = require("../db");
-async function databaseJob() {
+async function deleteUnverifiedUsers24hrs() {
   await db.query(`DELETE FROM users
                   WHERE verified = false
                   AND created_at < current_timestamp - interval '1 day'`);
   return "Job is done!";
 }
 
-module.exports = { databaseJob };
+module.exports = { deleteUnverifiedUsers24hrs };
